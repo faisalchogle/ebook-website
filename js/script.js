8fb6ff7398a14c8f1bbea7afe7d8d82fb02120a3
duke fleed <dukefleed@testinggit.com>
2024-03-09 15:48:39 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function userScroll(params) {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+      navbar.classList.add('bg-dark');
+      navbar.classList.add('navbar-sticky');
+    } else {
+      navbar.classList.remove('bg-dark');
+      navbar.classList.remove('navbar-sticky');
+    }
+  });
+}
+document.addEventListener('DOMContentLoaded', userScroll);
